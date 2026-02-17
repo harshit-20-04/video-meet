55f724413cb5abcc657a2e9d952832341e2b06fd
harshit-20-04 <h123afd@gmail.com>
2026-02-17 14:03:09 +0530

    Bugs fixed Properly

--- frontend/src/pages/NavBar.jsx
+++ frontend/src/pages/NavBar.jsx
@@ -1,15 +1,16 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
 
 export const NavBar = () => {
   return (
     <Navbar>
       <div className='nav-container'>
-        <Navbar.Brand href="#home" className='text-white brand-name'>VideoMeet</Navbar.Brand>
+        <Link to='/' className='text-white brand-name'>VideoMeet</Link>
         <Navbar id='basic-navbar-nav' className='navbar-collapse'>
           <Nav className="me-auto">
-            <Nav.Link href="/register" className='text-white'>Register</Nav.Link>
-            <Nav.Link href="/login" className='text-white'>Login</Nav.Link>
+            <Link to={'/register'} className='text-white nav-link'>Register</Link>
+            <Link to={'/login'} className='text-white nav-link'>Login</Link>
           </Nav>
         </Navbar>
       </div>
